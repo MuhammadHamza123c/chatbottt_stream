8c74ecae14ba638e60d355405f35eab77d3c8e59
Muhammad Hamza <muhammadhamzao241@gmail.com>
2025-07-06 14:36:37 +0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 user=st.text_input("Write down query here: ")
 if user:
- url=f"https://8201-103-172-166-163.ngrok-free.app/Chatbot?user={user}"
+ url=f"https://e095-103-172-166-163.ngrok-free.app/Chatbot?user={user}"
  response=requests.get(url)
  data=response.json()
  if data:
